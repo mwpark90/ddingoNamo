ad74f1336c463c0014e85cd4d046b68f9b805b32
ddingo <alsdn0528@gmail.com>
2023-04-19 22:21:22 +0900

    key 변경

--- namoddingo/fc_django/fc_django/settings.py
+++ namoddingo/fc_django/fc_django/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)*y0w_=46b*%=zr0(!=afl@un45)19(nh_6&go9-=^v)-(3oo_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
